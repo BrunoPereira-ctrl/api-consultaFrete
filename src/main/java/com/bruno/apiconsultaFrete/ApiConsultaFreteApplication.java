9a923d3a5f3bdc7b8213b8567870c282ce8062c9
Bruno Pereira <brunopereiradosreis369@gmail.com>
2023-03-30 14:34:25 -0300

    first commit

---
+++ src/main/java/com/bruno/apiconsultaFrete/ApiConsultaFreteApplication.java
@@ -0,0 +1,13 @@
+package com.bruno.apiconsultaFrete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConsultaFreteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConsultaFreteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bruno/apiconsultaFrete/ApiConsultaFreteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.apiconsultaFrete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConsultaFreteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
